d93581d875daa7ac9e28e9549d6b369e6f671911
ChristianPSlayer <chiki0930@gmail.com>
2026-04-16 19:00:50 -0400

    week06Done

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,8 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+            return; // No duplicates allowed in this tree
 
         if (value < Data)
         {
@@ -34,12 +36,34 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+            if (value == Data)
+                return true;
+        if (value < Data)
+        {
+            // Search to the left
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else
+        {
+            // Search to the right
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+            int leftHeight = Left?.GetHeight() ?? 0;
+            int rightHeight = Right?.GetHeight() ?? 0;
+            return 1 + Math.Max(leftHeight, rightHeight);
+
+      
     }
 }
